88cc50df16a53419fbeb5996c459e39d0114ba2a
Carlos Laurine <carlos.pinho@ucsal.edu.br>
2021-05-17 16:25:24 -0300

    Complete Interface + Email Forwarder Back End + Servlet Controller Implemented and Tested + Back End Development Comments

---
+++ src/main/java/email_forwarder/EmailForwarder.java
@@ -0,0 +1,106 @@
+package email_forwarder;
+
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailForwarder {
+
+	private String userName = "vitorlaurinepinho@gmail.com";
+	private String password = "Megadeth@3625";
+
+	private String addressesList = "carlos.pinho@ucsal.edu.br, cvpinhofsa@gmail.com, vitorlaurinepinho@gmail.com";
+	private String senderName = "Carlos Laurine - Java Web Developer";
+	private String emailSubject = "Test Java Email with javax.mail";
+	private String emailText = "Hello, this is a Default Test Email from Carlos Laurine!!";
+
+	public EmailForwarder() {
+
+	}
+
+	public EmailForwarder(String addressesList, String senderName, String emailSubject, String emailText) {
+		this.addressesList = addressesList;
+		this.senderName = senderName;
+		this.emailSubject = emailSubject;
+		this.emailText = emailText;
+	}
+
+	/*
+	 * Setting the Sending Method with a boolean Parameter to define whether the
+	 * email will have HTML content or not, since the sending process will vary
+	 * according to this information
+	 */
+
+	public void sendEmail(boolean containsHTML) throws Exception {
+
+		Properties properties = new Properties();
+		properties.put("mail.smtp.auth", "true"); // Authorization
+		properties.put("mail.smtp.starttls", "true"); // Authentication
+		properties.put("mail.smtp.host", "smtp.gmail.com"); // Setting Email Server (Google's Gmail)
+		properties.put("mail.smtp.port", "465");// Setting Server Port (Standard Google Port 465)
+		properties.put("mail.smtp.socketFactory.port", "465");// Specifying the Port to be Connected by the Socket
+		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Defining the Socket SMTP
+																							// Connection Class
+
+		// Setting an additional Security Authentication Property in order to match
+		// Gmail Server's recent Demand Updates
+
+		properties.put("mail.smtp.ssl.trust", "*"); // Setting Authentication with SSL Security
+
+		// Setting javax.mail Session object to perform the Connection (Obtaining a
+		// Session Object allowed to send Emails) through receiving the properties
+		// Object
+
+		Session session = Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+
+				return new PasswordAuthentication(userName, password);
+
+			}
+
+		});
+
+		// Setting all the Email Addresses to which the mail will be sent in an
+		// Address-type Array with a String using Colons as Separators
+
+		Address[] targetMails = InternetAddress.parse(addressesList);
+
+		// Linking the Message object with the Connection Session
+
+		Message msg = new MimeMessage(session);
+
+		// Building Message Configs
+
+		msg.setFrom(new InternetAddress(userName, senderName)); // Setting the Sender (OBS: The second String Parameter
+																// at InternetAddress' Constructor is the Sender name
+																// which will appear at the Recipients' Mailbox)
+
+		msg.setRecipients(Message.RecipientType.TO, targetMails); // Setting the Recipients
+
+		msg.setSubject(emailSubject); // Setting the Email Subject ("CC:" or Header)
+
+		// Performing the Message Object (Email) Submission
+
+		if (containsHTML) {
+			msg.setContent(emailText, "text/html; charset = utf-8"); // Setting the Message Content with the emailText
+																		// wrapped in a DataHandler for the specified
+																		// type (HTML)
+		} else {
+			msg.setText(emailText);// Setting the Email PURE Text Content
+		}
+
+		// E-mailing the Message (Forwarding)
+		Transport.send(msg);
+
+	}
+
+}
---
+++ src/main/java/servlets/MailServlet.java
@@ -0,0 +1,121 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import email_forwarder.EmailForwarder;
+
+@WebServlet("/mailInfo")
+public class MailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public MailServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+		String msgMail = "";
+			
+		if (action.equals("receive")) {
+			// First, verify the SMTP settings of the test email account
+
+			try {
+
+				// Setting StringBuilder object to Build the Email Content with inner HTML
+				StringBuilder emailTxtSB = new StringBuilder();
+
+				emailTxtSB.append("<h3>Hi, <b>" + request.getParameter("name")
+						+ "</b>!</h3> <br/> <h4>It is a pleasure to meet you. Let me introduce myself: <br/><br/><br/> I'm Carlos Pinho, a Braziian Independent <bold>Java Developer</bold> and <bold>Software Engineering Student</bold></h4> <br>");
+				emailTxtSB.append(
+						"<h4>I suppose you much probably have already seen my <b><a target=\"_blank\" href = \"CarlosLaurine.github.io\" style=\"color: #dc143c; text-decoration: none; \"> Personal Page </a></b>, so let's step up to some Additional Info:</h4>  <br>");
+				emailTxtSB.append("<h4>You are now receiving Access to my Portfolio!</h4><br>");
+				emailTxtSB.append(
+						"<a target=\"_blank\" href = \"https://pdfhost.io/v/poS2vqMxr_SEBRAE_Certificatepdf.pdf\" style=\"color: #2525a7; padding:14px 25px; text-align: center; text-decoration: none; display: inline-block; border-radius:15px; font-size:25px; font-family:courier; border: 4px solid black; background-color:#dc143c \"> Access my Resume </a> ");
+				emailTxtSB.append(
+						"<a target=\"_blank\" href = \"https://www.linkedin.com/in/carlos-laurine-a58267144\" style=\"color: #2525a7; padding:14px 25px; text-align: center; text-decoration: none; display: inline-block; border-radius:15px; font-size:25px; font-family:courier; border: 4px solid black; background-color:#dc143c \"> Access my LinkedIn </a><br/><br/> ");
+				emailTxtSB.append(
+						"<a target=\"_blank\" href = \"CarlosLaurine.github.io\" style=\"color: #2525a7; padding:14px 25px; text-align: center; text-decoration: none; display: inline-block; border-radius:15px; font-size:25px; font-family:courier; border: 4px solid black; background-color:#dc143c \"> Access my GitHub </a>");
+				
+				emailTxtSB.append(
+						"<a target=\"_blank\" href = \"https://www.instagram.com/cv_laurine/?hl=pt-br\" style=\"color: #2525a7; padding:14px 25px; text-align: center; text-decoration: none; display: inline-block; border-radius:15px; font-size:25px; font-family:courier; border: 4px solid black; background-color:#dc143c \"> Access my Instagram </a><br/><br/><br/>");
+
+				// Setting Virtual Signature at the Email Footer
+
+				emailTxtSB.append(
+						"<b><span style=\"font-size:12px\">Att.: Carlos Laurine - Java Web Fullstack Developer</span></b><br/>");
+				emailTxtSB.append("<b><span style=\"font-size:12px\">Contact Number - +55 (71) 99318-7816 </span></b><br/><br/>");
+
+				emailTxtSB.append("Sent to you by JavaMail 1.6.2");
+
+				EmailForwarder forwarder = new EmailForwarder(request.getParameter("email"),
+						"Carlos Laurine - Freelancer Java Web Fullstack Dev", "Java Web Fullstack Developer's Page",
+						emailTxtSB.toString());
+				forwarder.sendEmail(true);
+				msgMail="Email Succesfully Sent! I'll be in touch Soon!";
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				msgMail="Invalid Mail Address! Please Re-Insert your Info";
+				
+				
+			}
+
+		}
+
+		else if (action.equals("send")) {
+
+			try {
+
+				// Setting StringBuilder object to Build the Email Content with inner HTML
+				StringBuilder emailTxtSB = new StringBuilder();
+
+				emailTxtSB.append("<h3>Hi, <b>" + "Carlos!" + "</b>!</h3> <b> " + request.getParameter("name")
+						+ "</b> <span style=\"color: #2525a7;\">wants to contact you!<span/> <br/> <h4>Email Subject:<h4/> <span style=\"color: #2525a7;\">"
+						+ request.getParameter("subject")
+						+ "<span/> <br/> <h4>Email Content:</h4> <span style=\"color: #2525a7;\">"
+						+ request.getParameter("textbody") + "<span/> <br/><br/><br/>");
+
+				// Setting Virtual Signature at the Email Footer
+
+				emailTxtSB.append("<b><span style=\"font-size:12px; color: #2525a7;\">Sent By: "
+						+ request.getParameter("email") + "</span></b><br/>");
+
+				emailTxtSB.append(
+						"<span style=\"color: #2525a7; text-style:italic;\">Sent to you by JavaMail 1.6.2<span/>");
+
+				EmailForwarder forwarder = new EmailForwarder("vitorlaurinepinho@gmail.com",
+						request.getParameter("name"), request.getParameter("subject"), emailTxtSB.toString());
+				forwarder.sendEmail(true);
+				
+				msgMail="Email Succesfully Sent! Go check your Mailbox";
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				msgMail="Invalid Mail Address! Please Re-Insert your Info";
+			}
+
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		request.setAttribute("mailMsg", msgMail);
+		rd.forward(request, response);
+		
+	}
+}
